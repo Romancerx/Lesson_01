f8102d92e7962119b243181084bbd3cfb163eb24
Роман Костенецкий <romancerx@yandex.ru>
2021-01-16 09:37:59 +0300

    First commit for Task_3

---
+++ Task_3.py
@@ -0,0 +1,16 @@
+# Узнайте у пользователя число n.
+# Найдите сумму чисел n + nn + nnn.
+# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+# Request some number
+user_number = int(input("Введите число: "))
+# Formatting "nn" and "nnn"
+nn = '%d''%d' % (user_number, user_number)
+nnn = '%d''%d''%d' % (user_number, user_number, user_number)
+# Printing result
+print(int(user_number) + int(nn) + int(nnn))
+
+# Print in one string
+print(int(user_number)
+      + int('%d''%d' % (user_number, user_number))
+      + int('%d''%d''%d' % (user_number, user_number, user_number)))
