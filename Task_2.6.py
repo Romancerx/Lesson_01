d11e88ddb5348f92d73aa33ce7e8dcfb6bca8d51
Роман Костенецкий <romancerx@yandex.ru>
2021-01-18 23:55:29 +0300

    Correcting skeleton

--- Task_2.6.py
+++ Task_2.6.py
@@ -33,9 +33,9 @@ while True:
     if if_add_new == 'No':
         print(f'List of goods: {goods_list}')
         break
-    worker_dict['title'] = input("Title: ")
-    worker_dict['price'] = input("Price: ")
+    goods_dict['title'] = input("Title: ")
+    goods_dict['price'] = input("Price: ")
     # worker_dict['vaccine'] = bool(input("Vaccine?: ")) #не рабочий вариант
-    worker_dict['quantity'] = input("Quantity: ")  # работает проверка со строкой
-    worker_dict['pieces'] = float(input("Pieces: "))
+    goods_dict['quantity'] = input("Quantity: ")  # работает проверка со строкой
+    goods_dict['pieces'] = float(input("Pieces: "))
     goods_list.append(goods_dict.copy())
