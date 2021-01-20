69bd125c4759df6f5c3b987f9de95b73aad8bb94
Роман Костенецкий <romancerx@yandex.ru>
2021-01-20 22:13:25 +0300

    Optimized

--- Task_2.4.py
+++ Task_2.4.py
@@ -5,7 +5,7 @@
 user_str = input("Введите фразу: ")  # Create string
 my_list = (user_str.split())  # Create list of words from user string
 for el in range(0, len(my_list)):
-    if len(my_list[el]) < 10:
-        print(f"{el.numerator + 1} {my_list[el]}")  # Print word if it lower than 10 letters
-    else:
-        print(f"{el.numerator + 1} {my_list[el][:10]}")  # Print only 10 letters if word bigger than 10 letters
+#    if len(my_list[el]) < 10:
+#        print(f"{el.numerator + 1} {my_list[el]}")  # Print word if it lower than 10 letters
+#    else:
+     print(f"{el.numerator + 1} {my_list[el][:10]}")  # Print only 10 letters if word bigger than 10 letters
