50ca0774e9855e92ef85188a2d07d6eea1a32c6a
Роман Костенецкий <romancerx@yandex.ru>
2021-01-24 16:29:07 +0300

    Finished Task_3.5

--- Task_3.5.py
+++ Task_3.5.py
@@ -6,26 +6,29 @@
 # Если специальный символ введен после нескольких чисел,
 # то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
 
+# global vars
 end_flag = 1
 result = 0
 
+
 def find_spaces_func(my_string):
-    num = ""
+    num = ""  # Clear "num" var
     global end_flag, result
 
     for i in range(0, len(my_string)):
-        if my_string[i] != " " and my_string[i] != "Q":
-             num += my_string[i]
-        elif my_string[i] == "Q":
-             end_flag = 0
-             break
+        if my_string[i] != " " and my_string[i] != "Q":  # Find spaces and char Q
+            num += my_string[i]  # Add symbols before space found
+        elif my_string[i] == "Q":  # Exit if Q symbol found
+            end_flag = 0  # Flag for stop programm
+            break
         else:
-             result += int(num)
-             num = ""
-        if i == len(my_string) - 1:
-             result += int(num)
-             num = ""
+            result += int(num)  # Sum numbers
+            num = ""  # Clean old number
+        if i == len(my_string) - 1:  # Adding last symbol
+            result += int(num)
+            num = ""
     print(f"Сумма всех чисел равна: {result}")
+
 
 while end_flag != 0:
     find_spaces_func(input("Введите числа через пробел, символ Q для выхода: "))
