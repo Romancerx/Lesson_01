4667af346f42299d4803a4260903100bda7e8457
Роман Костенецкий <romancerx@yandex.ru>
2021-01-25 23:06:42 +0300

    Finished Task_4.1. Command for launch script is "python" without "3"

--- Task_4.1.py
+++ Task_4.1.py
@@ -2,20 +2,15 @@
 # В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия.
 # Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 
-# from argparse import ArgumentParser
-#
-# parser = ArgumentParser()
-#
-# parser.add_argument('--work_hours', type=int)
-# parser.add_argument('--hour_price', type=int)
-# parser.add_argument('--bonus', type=int, default=None)
-#
-#
-# args = parser.parse_args()
-# print(f"Заработная плата сотрудника равна {args.work_hours}.")
+from argparse import ArgumentParser
 
-from sys import argv
+parser = ArgumentParser()
 
-#print(f'Our args is {argv}. It is type {type(argv)}, {type(argv[-1])}')
-script_name, wh, hp, bonus = argv
-print(f'Hello, worker {wh}*{hp}!')
+parser.add_argument('--wh', type=int)  # working_hours
+parser.add_argument('--hp', type=int)  # hour_price
+parser.add_argument('--b', type=int, default=None)  # bonus
+
+args = parser.parse_args()
+salary = args.wh * args.hp + args.b  # count salary
+print(f"Заработная плата сотрудника равна {salary}.")  # print salary
+
