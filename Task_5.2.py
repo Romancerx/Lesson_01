806dd951be047a8c135d0e45db9237996393edbe
Роман Костенецкий <romancerx@yandex.ru>
2021-01-29 01:10:48 +0300

    Finished Task_5.2

--- Task_5.2.py
+++ Task_5.2.py
@@ -1,17 +1,21 @@
 # Создать текстовый файл (не программно), сохранить в нём несколько строк,
 # выполнить подсчёт строк и слов в каждой строке.
 
-my_f = open("out_file.txt", "r")
-my_str = []
-lines_counter = 0
-words_counter = 0
-line_str = []
+# Open file for reading
+my_f = open("out_file.txt", "r", encoding="UTF-8")
+lines_counter = 0  # Strings counter
+
 for line in my_f:
     lines_counter += 1
-    my_str.append(line)
-    words_counter += my_str.count(" ") + 1
-    
-    #print(line)
-print(my_str)
-print(lines_counter, words_counter)
-my_f.close()
+    my_list = line.split(" ")  # Create list of words to find extra spaces
+
+    while my_list.count("") > 0:  # remove extra spaces between words
+        my_list.remove("")
+
+    print(f"Количество слов в {lines_counter}-й строке: {len(my_list)}")
+
+my_f.close()  # Close file
+print(f"Всего строк: {lines_counter}")
+
+# По идее нужно проверять нет ли между пробелами лишних символов. Я проверил только на лишние пробелы.
+# Как красиво проверить на все символы не придумал. Перебирать все ASCII коды сложно, думаю есть простой способ.
