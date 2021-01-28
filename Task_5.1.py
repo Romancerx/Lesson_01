ca50b790d7e78d28b9e2ee352728fec3c43c8f78
Роман Костенецкий <romancerx@yandex.ru>
2021-01-28 23:28:00 +0300

    Finished Task_5.1

--- Task_5.1.py
+++ Task_5.1.py
@@ -2,11 +2,10 @@
 # записать в него построчно данные, вводимые пользователем.
 # Об окончании ввода данных будет свидетельствовать пустая строка.
 
-out_f = open("out_file.txt", "w")
+out_f = open("new_file.txt", "w")  # Create file, for writing
 while True:
     user_str = input("Введите строку: ")
-    if user_str == "":
+    if user_str == "":  # stop condition
         break
-    out_f.write(user_str + "\n")
-    
-out_f.close()
+    out_f.write(user_str + "\n")  # add new string
+out_f.close()  # close file
